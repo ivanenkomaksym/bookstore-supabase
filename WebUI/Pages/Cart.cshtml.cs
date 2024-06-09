7eea86642f4156f30b2ece6c5be52a8f4e417703
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-06-09 12:30:04 +0300

    Update UI to support adding products to cart.

---
+++ WebUI/Pages/Cart.cshtml.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebUI.Models;
+using WebUI.Services;
+
+namespace WebUI.Pages
+{
+    public class CartModel : PageModel
+    {
+        private readonly IShoppingCartService _shoppingCartService;
+        private readonly IAuthService _authService;
+
+        public CartModel(IShoppingCartService shoppingCartService, IAuthService authService)
+        {
+            _shoppingCartService = shoppingCartService;
+            _authService = authService;
+        }
+
+        [BindProperty]
+        public ShoppingCart Cart { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            var user = await _authService.GetUser();
+
+            Cart = await _shoppingCartService.GetShoppingCart(user.Id);
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostIncreaseQuantity(int itemId, ushort quantity)
+        {
+            var user = await _authService.GetUser();
+
+            Cart = await _shoppingCartService.GetShoppingCart(user.Id);
+
+            // Find the item in the shopping cart
+            var item = Cart.CartItems.FirstOrDefault(i => i.Id == itemId);
+            if (item != null)
+            {
+                // Increase the quantity
+                item.Quantity = quantity;
+
+                // Update the total price of the shopping cart
+                Cart.TotalPrice = CalculateTotalPrice();
+            }
+
+            var basketUpdated = await _shoppingCartService.UpdateShoppingCart(Cart);
+
+            return Page();
+        }
+
+        private double CalculateTotalPrice()
+        {        
+            // Calculate the total price based on the items in the shopping cart
+            return Cart.CartItems.Sum(item => item.ProductPrice * item.Quantity);
+        }
+
+        public async Task<IActionResult> OnPostRemoveToCartAsync(int productId)
+        {
+            var user = await _authService.GetUser();
+
+            var cart = await _shoppingCartService.GetShoppingCart(user.Id);
+
+            var item = cart.CartItems.Single(x => x.ProductId == productId);
+            cart.CartItems.Remove(item);
+
+            var basketUpdated = await _shoppingCartService.UpdateShoppingCart(cart);
+
+            return RedirectToPage();
+        }
+    }
+}
--- WebUI/Pages/Index.cshtml.cs
+++ WebUI/Pages/Index.cshtml.cs
@@ -8,11 +8,15 @@ namespace WebUI.Pages
     public class IndexModel : PageModel
     {
         private readonly IProductService _productService;
+        private readonly IShoppingCartService _shoppingCartService;
+        private readonly IAuthService _authService;
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(IProductService productService, ILogger<IndexModel> logger)
+        public IndexModel(IProductService productService, IShoppingCartService shoppingCartService, IAuthService authService, ILogger<IndexModel> logger)
         {
             _productService = productService;
+            _shoppingCartService = shoppingCartService;
+            _authService = authService;
             _logger = logger;
         }
 
@@ -23,6 +27,17 @@ namespace WebUI.Pages
             ProductList = await _productService.GetProducts();
 
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAddToCartAsync(int productId)
+        {
+            var product = await _productService.GetProduct(productId);
+
+            var user = await _authService.GetUser();
+
+            var cart = await _shoppingCartService.AddProductToCart(user.Id, product);
+
+            return RedirectToPage("Cart");
         }
     }
 }
